6a387c2d3690322bfab9bba78574363df2d0bb86
Nahun Silva <nahuns@gmail.com>
2024-10-28 20:13:07 +0000

    Added missing namespace

--- backend/start.sh
+++ backend/start.sh
@@ -2,19 +2,17 @@
 
 set -Eeuo pipefail
 
+composer install -v --optimize-autoloader --no-scripts \
+    && php artisan cache:clear \
+    && php artisan config:clear \
+    && php artisan view:clear
+
 # Define the available options
 serve_command="php artisan serve --host=0.0.0.0"
 queue_command="php artisan queue:work --tries=3"
 
 # Check the command-line arguments
 if [[ "$1" == "--serve" ]]; then
-    echo "Installing composer dependencies..."
-    composer dump-autoload \
-        && composer install --optimize-autoloader --no-dev \
-        && php artisan cache:clear \
-        && php artisan config:clear \
-        && php artisan view:clear
-
     echo "Running migrations..."
 
     php artisan migrate
@@ -23,7 +21,7 @@ if [[ "$1" == "--serve" ]]; then
 
     php artisan db:seed
 
-    echo "Starting server..."
+    echo "Executing: $serve_command"
     $serve_command \
         && echo "... and we are ready!!! Visit the ToDoist application on http://localhost:8001"
 elif [[ "$1" == "--queue" ]]; then

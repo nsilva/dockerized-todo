5243cedf303754a5de32c558f7a797d4ab84fefe
Nahun Silva <nahuns@gmail.com>
2024-10-28 15:36:10 +0000

    Updated to Dockerfile and add .env file to repo to facilitate app running/testing

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -22,7 +22,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('password123'), // password
+            'password' => Hash::make('password123'),
             'remember_token' => Str::random(10),
         ];
     }
